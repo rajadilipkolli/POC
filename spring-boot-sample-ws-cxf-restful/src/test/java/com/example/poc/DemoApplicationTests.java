1122acb6410ffece3d3ae9ebd59c96d3850e70f3
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2015-12-13 16:03:49 +0530

    Converted code to Maven modules and polished code to use parent pom

--- spring-boot-sample-ws-cxf-restful/src/test/java/com/example/poc/DemoApplicationTests.java
+++ spring-boot-sample-ws-cxf-restful/src/test/java/com/example/poc/DemoApplicationTests.java
@@ -1,10 +1,12 @@
-package com.example.sample;
+package com.example.poc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.poc.DemoApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
