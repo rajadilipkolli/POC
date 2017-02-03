2699e567a3c4761f9f2c2a71921b9d013bc54e4b
rajadilipkolli <rajadilipkolli@gmail.com>
2017-02-03 15:30:45 +0530

    Adds autoconfiguration nature for JAX-RS using apache-cxf
    Note :: this will work when apache-cxf- 3.2.0 is released
    added swagger-ui displayable

--- spring-boot-sample-ws-cxf-restful/src/main/java/com/example/poc/restful/controller/HelloServiceImpl1.java
+++ spring-boot-sample-ws-cxf-restful/src/main/java/com/example/poc/restful/controller/HelloServiceImpl1.java
@@ -1,12 +1,23 @@
 package com.example.poc.restful.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api("/sayHello")
-public class HelloServiceImpl1 implements HelloService {
- 
-    public String sayHello(String a) {
-        return "Hello " + a + ", Welcome to CXF RS Spring Boot World!!!";
+public class HelloServiceImpl1 implements HelloService
+{
+
+    @Override
+    @ApiOperation(httpMethod = "GET", value = "Ping User", notes = "This acts as prototype for rest API.", response = String.class)
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Invalid username supplied"),
+            @ApiResponse(code = 404, message = "UserName not found") })
+    public String sayHello(@ApiParam(value = "userName", required = true) String userName)
+    {
+        return "Hello " + userName + ", Welcome to CXF RS Spring Boot World!!!";
     }
-    
+
 }
