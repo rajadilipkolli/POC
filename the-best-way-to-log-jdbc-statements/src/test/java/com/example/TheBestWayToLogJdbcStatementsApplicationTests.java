86ed50672dfc126b5cd44a5866c12aeb1fae8819
rajadilipkolli <rajadilipkolli@gmail.com>
2017-04-04 17:07:21 +0530

    increments spring-boot version and adds JUNIT test case for JBDC logs

--- the-best-way-to-log-jdbc-statements/src/test/java/com/example/TheBestWayToLogJdbcStatementsApplicationTests.java
+++ the-best-way-to-log-jdbc-statements/src/test/java/com/example/TheBestWayToLogJdbcStatementsApplicationTests.java
@@ -1,17 +1,30 @@
-/*package com.example;
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TheBestWayToLogJdbcStatementsApplicationTests {
+public class TheBestWayToLogJdbcStatementsApplicationTests
+{
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException
+    {
+        assertThat(dataSource).isNotNull();
+        assertThat(dataSource.getConnection().getSchema()).isNotEmpty();
+    }
 
 }
-*/
