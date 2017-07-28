e083cbff0b484f9fc53b1ba8c9d5471df84053df
rajadilipkolli <rajadilipkolli@gmail.com>
2017-07-29 02:11:39 +0530

    Upgrades to M3 and let springboot create cacheManager
    Fixes Checkstyle submodule build failure issue

--- mongodb-redis-integration/src/main/java/com/poc/mongodbredisintegration/config/RedisConfiguration.java
+++ mongodb-redis-integration/src/main/java/com/poc/mongodbredisintegration/config/RedisConfiguration.java
@@ -10,9 +10,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * @author rajakolli
@@ -23,49 +20,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 @EnableCaching
 @Configuration
 public class RedisConfiguration extends CachingConfigurerSupport {
-
-    /**
-     * <p>jedisConnectionFactory.</p>
-     *
-     * @return a {@link org.springframework.data.redis.connection.jedis.JedisConnectionFactory} object.
-     */
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    /* redis template definition */
-    /**
-     * <p>redisTemplate.</p>
-     *
-     * @return a {@link org.springframework.data.redis.core.RedisTemplate} object.
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
-
-    /*
-     * declare Redis Cache Manager
-     * 
-     * Never load remote caches on startup as it can be corrupted
-     * 
-     * (non-Javadoc)
-     * 
-     * @see org.springframework.cache.annotation.CachingConfigurerSupport#cacheManager()
-     */
-    /** {@inheritDoc} */
-    @Bean
-    @Override
-    public RedisCacheManager cacheManager() {
-        final RedisCacheManager redisCacheManager = new RedisCacheManager(
-                redisTemplate());
-        redisCacheManager.setTransactionAware(true);
-        // redisCacheManager.setDefaultExpiration(10); /*Enabled for Testing Purposes*/
-        return redisCacheManager;
-    }
 
     /*
      * (non-Javadoc)
