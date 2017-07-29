a14a7f7439dc2b9deda7b9c02310cc2b5fa7c006
rajadilipkolli <rajadilipkolli@gmail.com>
2017-07-29 11:31:54 +0530

    adds datasourceproxy

---
+++ spring-boot-rest-poc/src/main/java/com/poc/restfulpoc/config/DataSourceProxyBeanConfig.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) Raja Dilip Chowdary Kolli. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.poc.restfulpoc.config;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import net.ttddyy.dsproxy.listener.logging.SystemOutQueryLoggingListener;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
+@Slf4j
+@Configuration
+public class DataSourceProxyBeanConfig implements BeanPostProcessor {
+
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+
+        if (bean instanceof DataSource) {
+            System.out.println("AfterInitialization : " + beanName);
+
+            log.info("Inside Proxy Creation");
+
+            final SystemOutQueryLoggingListener listener = new SystemOutQueryLoggingListener();
+
+            return ProxyDataSourceBuilder.create((DataSource) bean).countQuery()
+                    .name("MyDS").listener(listener).multiline()
+                    .logSlowQueryToSysOut(1, TimeUnit.MINUTES).build();
+
+        }
+        return bean; // you can return any other object as well
+    }
+}
