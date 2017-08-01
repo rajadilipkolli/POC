1b41bacea2ba6860ee2459bef57ff2d6342f8fa8
rajadilipkolli <rajadilipkolli@gmail.com>
2017-08-01 09:25:19 +0530

    fixes build issue from submodules

--- mongodb-redis-integration/src/test/java/com/poc/mongodbredisintegration/AbstractMongoDBRedisIntegrationTest.java
+++ mongodb-redis-integration/src/test/java/com/poc/mongodbredisintegration/AbstractMongoDBRedisIntegrationTest.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Raja Dilip Chowdary Kolli. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
 package com.poc.mongodbredisintegration;
 
 import org.junit.runner.RunWith;
@@ -6,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = MongoDBRedisIntegrationApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AbstractMongoDBRedisIntegrationTest {
 
 }
