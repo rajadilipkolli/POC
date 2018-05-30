d0d9f8bd042354895c4c3307526c31e0bce1a572
rajadilipkolli <rajadilipkolli@gmail.com>
2018-05-30 18:31:34 +0530

    attempt to fix travis build failure

--- spring-boot-rest/src/main/java/com/poc/restfulpoc/exception/LowerCaseClassNameResolver.java
+++ spring-boot-rest/src/main/java/com/poc/restfulpoc/exception/LowerCaseClassNameResolver.java
@@ -30,21 +30,18 @@ import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
 class LowerCaseClassNameResolver extends TypeIdResolverBase {
 
 	/** {@inheritDoc} */
-
 	@Override
 	public String idFromValue(Object value) {
 		return value.getClass().getSimpleName().toLowerCase(Locale.getDefault());
 	}
 
 	/** {@inheritDoc} */
-
 	@Override
 	public String idFromValueAndType(Object value, Class<?> suggestedType) {
 		return idFromValue(value);
 	}
 
 	/** {@inheritDoc} */
-
 	@Override
 	public JsonTypeInfo.Id getMechanism() {
 		return JsonTypeInfo.Id.CUSTOM;
