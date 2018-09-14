7ca8a6754f874b9a6f824fe09bea7558a9a85f28
rajadilipkolli <rajadilipkolli@gmail.com>
2018-09-14 20:10:43 +0530

    adds fresh project from starch
    Removes previous code and adds new one

---
+++ src/main/java/com/example/poc/pocparent/PocParentApplication.java
@@ -0,0 +1,12 @@
+package com.example.poc.pocparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocParentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/poc/pocparent/PocParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.poc.pocparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
