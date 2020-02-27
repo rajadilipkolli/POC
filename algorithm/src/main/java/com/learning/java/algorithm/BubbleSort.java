752daf82d0e9583011e8cfeff85975ce6795a30f
rajadilipkolli <rajadilipkolli@gmail.com>
2020-02-27 19:48:25 +0530

    adds algorithms

--- algorithm/src/main/java/com/learning/java/algorithm/BubbleSort.java
+++ algorithm/src/main/java/com/learning/java/algorithm/BubbleSort.java
@@ -1,4 +1,37 @@
 package com.learning.java.algorithm;
 
+import java.util.Arrays;
+
+/**
+ * Time Complexity is O(n2) , not preferable and it is stable sort.
+ */
 public class BubbleSort {
+
+  public static void main(String[] args) {
+    int[] arr = new int[7];
+    arr[0] = 20;
+    arr[1] = 35;
+    arr[2] = -15;
+    arr[3] = 7;
+    arr[4] = 55;
+    arr[5] = 1;
+    arr[6] = -22;
+
+    bubbleSort(arr, arr.length);
+  }
+
+  private static void bubbleSort(int[] arr, int lastUnsortedPartitionIndex) {
+    if (lastUnsortedPartitionIndex > 1) {
+      for (int i = 0; i < lastUnsortedPartitionIndex - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          int temp = arr[i + 1];
+          arr[i + 1] = arr[i];
+          arr[i] = temp;
+        }
+      }
+      lastUnsortedPartitionIndex--;
+      System.out.println(Arrays.toString(arr));
+      bubbleSort(arr, lastUnsortedPartitionIndex);
+    }
+  }
 }
--- algorithm/src/main/java/com/learning/java/algorithm/SelectionSort.java
+++ algorithm/src/main/java/com/learning/java/algorithm/SelectionSort.java
@@ -1,4 +1,45 @@
 package com.learning.java.algorithm;
 
+import java.util.Arrays;
+
+/**
+ * Time Complexity is O(n2) , not preferable and it is unstable sort but better than BubbleSort as number of swaps reduce.
+ * Number of Iterations = length -2;
+ */
 public class SelectionSort {
+
+  public static void main(String[] args) {
+    int[] arr = new int[7];
+    arr[0] = 20;
+    arr[1] = 35;
+    arr[2] = -15;
+    arr[3] = 7;
+    arr[4] = 55;
+    arr[5] = 1;
+    arr[6] = -22;
+
+    selectionSort(arr);
+  }
+
+  private static void selectionSort(int[] arr) {
+    for (int largestUnSortedIndex = arr.length - 1; largestUnSortedIndex > 1; largestUnSortedIndex--) {
+      int largestValue = 0;
+      for (int j = 1; j <= largestUnSortedIndex; j++) {
+        if (arr[largestValue] < arr[j]) {
+          largestValue = j;
+        }
+      }
+      swap(arr, largestValue, largestUnSortedIndex);
+      System.out.println(Arrays.toString(arr));
+    }
+  }
+
+  private static void swap(int[] arr, int highestIndex, int i) {
+    if (highestIndex == i) {
+      return;
+    }
+    int temp = arr[highestIndex];
+    arr[highestIndex] = arr[i];
+    arr[i] = temp;
+  }
 }
