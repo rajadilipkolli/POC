0cfbe8c9d103609f636b7278b8c49c34e1f6db54
rajadilipkolli <rajadilipkolli@gmail.com>
2020-04-09 16:22:03 +0530

    implemented routes for login, welcome and error components.

--- frontend/blog-angular-ui/src/app/app-routing.module.ts
+++ frontend/blog-angular-ui/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
 
-
-const routes: Routes = [];
+// welcome
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/blog-angular-ui/src/app/app.module.ts
+++ frontend/blog-angular-ui/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
