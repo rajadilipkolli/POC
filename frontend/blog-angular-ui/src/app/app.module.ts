f759783d149ad93a33122e57a58e64931fd6fc0c
rajadilipkolli <rajadilipkolli@gmail.com>
2020-04-09 16:08:37 +0530

    generated welcome component
    ng generate component welcome

--- frontend/blog-angular-ui/src/app/app.module.ts
+++ frontend/blog-angular-ui/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
