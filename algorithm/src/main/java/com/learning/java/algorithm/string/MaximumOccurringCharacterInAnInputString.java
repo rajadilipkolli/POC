c73115f427788addaa88dadfd19bc420727d5d31
Raja Kolli <rajadilipkolli@users.noreply.github.com>
2020-09-03 23:18:53 +0530

    adds algorithm to find first max value in string

--- algorithm/src/main/java/com/learning/java/algorithm/string/MaximumOccurringCharacterInAnInputString.java
+++ algorithm/src/main/java/com/learning/java/algorithm/string/MaximumOccurringCharacterInAnInputString.java
@@ -13,12 +13,35 @@ public class MaximumOccurringCharacterInAnInputString {
 	private static final int ASCII_SIZE = 256;
 
 	public static void main(String[] args) {
-		String str = "test";
+		String str = "helloworld";
 		System.out.println("Max occurring character is " + getMaxOccurringChar(str));
 
 		String anotherString = "test sample";
 		System.out.println("Max occurring character in lexicographically is"
 				+ getMaxOccurringCharsLexicographically(anotherString.toCharArray()));
+
+		anotherString = "bbaaabccc";
+		System.out.println("Max occurring character which occurs first is " + getMaxOccurringCharFirst(anotherString));
+	}
+
+	private static char getMaxOccurringCharFirst(String anotherString) {
+		int[] count = new int[ASCII_SIZE];
+		char result = anotherString.charAt(0);
+		int max = -1;
+		for (int i = 0; i < anotherString.length(); i++) {
+			count[anotherString.charAt(i)]++;
+			if (max < count[anotherString.charAt(i)]) {
+				max = count[anotherString.charAt(i)];
+			}
+		}
+
+		for (int i = 0; i < anotherString.length(); i++) {
+			if (max == count[anotherString.charAt(i)]) {
+				result = anotherString.charAt(i);
+				break;
+			}
+		}
+		return result;
 	}
 
 	private static List<Character> getMaxOccurringCharsLexicographically(char[] anotherString) {
