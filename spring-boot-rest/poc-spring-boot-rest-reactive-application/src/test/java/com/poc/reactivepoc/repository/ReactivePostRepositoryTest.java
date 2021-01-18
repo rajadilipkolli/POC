cf78db3afe2c8a241823c388b7035ba08648e2f4
Raja Kolli <rajadilipkolli@users.noreply.github.com>
2021-01-19 01:11:59 +0530

    removes deprecated package

--- spring-boot-rest/poc-spring-boot-rest-reactive-application/src/test/java/com/poc/reactivepoc/repository/ReactivePostRepositoryTest.java
+++ spring-boot-rest/poc-spring-boot-rest-reactive-application/src/test/java/com/poc/reactivepoc/repository/ReactivePostRepositoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.poc.reactivepoc.repository;
 
 import java.util.Arrays;
@@ -26,7 +27,7 @@ import reactor.test.StepVerifier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.core.DatabaseClient;
 
 @DataR2dbcTest
 class ReactivePostRepositoryTest {
