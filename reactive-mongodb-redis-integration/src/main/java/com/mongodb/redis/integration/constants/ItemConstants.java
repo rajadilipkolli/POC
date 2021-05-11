b857709ef01d77842a4fa906a1249af1aa658f1e
rajadilipkolli <rajadilipkolli@gmail.com>
2021-05-12 00:11:39 +0530

    adds more endpoints

--- reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/constants/ItemConstants.java
+++ reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/constants/ItemConstants.java
@@ -3,4 +3,5 @@ package com.mongodb.redis.integration.constants;
 public class ItemConstants {
 
   public static final String ITEM_END_POINT_V_1 = "/v1/items";
+  public static final String ITEM_FUNCTIONAL_END_POINT_V_1 = "/v1/fun/items";
 }
---
+++ reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/route/ItemsRouter.java
@@ -0,0 +1,40 @@
+package com.mongodb.redis.integration.route;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+import com.mongodb.redis.integration.constants.ItemConstants;
+import com.mongodb.redis.integration.handler.ItemsHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class ItemsRouter {
+
+  @Bean
+  public RouterFunction<ServerResponse> itemRouterFunction(ItemsHandler itemsHandler) {
+    return RouterFunctions.route(
+            GET(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V_1)
+                .and(accept(MediaType.APPLICATION_JSON)),
+            itemsHandler::getAllItems)
+        .andRoute(
+            GET(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V_1 + "/{id}")
+                .and(accept(MediaType.APPLICATION_JSON)),
+            itemsHandler::getItemById)
+        .andRoute(
+            POST(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V_1)
+                .and(accept(MediaType.APPLICATION_JSON)),
+            itemsHandler::createItem)
+        .andRoute(
+            DELETE(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V_1 + "/{id}")
+                .and(accept(MediaType.APPLICATION_JSON)),
+            itemsHandler::deleteItem)
+        .andRoute(
+            PUT(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V_1 + "/{id}")
+                .and(accept(MediaType.APPLICATION_JSON)),
+            itemsHandler::updateItem);
+  }
+}
