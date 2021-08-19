03b0be9521a9c8031264d7fff1d31490c7df5236
rajadileepkolli <rajadileepkolli@gmail.com>
2021-08-19 21:31:42 +0530

    fix rabbitMQ Sample issue

--- poc-spring-boot-rabbitmq/src/main/java/com/poc/boot/rabbitmq/config/OrderMessageListener.java
+++ poc-spring-boot-rabbitmq/src/main/java/com/poc/boot/rabbitmq/config/OrderMessageListener.java
@@ -3,8 +3,10 @@ package com.poc.boot.rabbitmq.config;
 import com.poc.boot.rabbitmq.model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
+@Configuration(proxyBeanMethods = false)
 public class OrderMessageListener {
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_ORDERS)
--- poc-spring-boot-rabbitmq/src/main/java/com/poc/boot/rabbitmq/model/Order.java
+++ poc-spring-boot-rabbitmq/src/main/java/com/poc/boot/rabbitmq/model/Order.java
@@ -4,9 +4,11 @@ import java.io.Serial;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Order implements Serializable {
 
     @Serial private static final long serialVersionUID = 1L;
