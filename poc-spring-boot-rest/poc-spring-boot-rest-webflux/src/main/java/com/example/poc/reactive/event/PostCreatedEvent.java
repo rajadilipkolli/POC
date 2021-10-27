5b5565461c90bf648dcc0870974476da38829cd2
rajadileepkolli <rajadileepkolli@gmail.com>
2021-10-27 23:36:23 +0530

    converts to native image

--- poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/event/PostCreatedEvent.java
+++ poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/event/PostCreatedEvent.java
@@ -1,12 +1,11 @@
 package com.example.poc.reactive.event;
 
 import com.example.poc.reactive.entity.ReactivePost;
-import java.io.Serial;
 import org.springframework.context.ApplicationEvent;
 
 public class PostCreatedEvent extends ApplicationEvent {
 
-    @Serial private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public PostCreatedEvent(ReactivePost post) {
         super(post);
--- poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/exception/PostNotFoundException.java
+++ poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/exception/PostNotFoundException.java
@@ -1,11 +1,10 @@
 package com.example.poc.reactive.exception;
 
-import java.io.Serial;
 import lombok.Getter;
 
 public class PostNotFoundException extends Exception {
 
-    @Serial private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Getter private final String message;
 
