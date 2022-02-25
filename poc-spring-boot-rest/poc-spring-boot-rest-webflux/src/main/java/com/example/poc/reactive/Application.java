d7a52893c83db7ae02e4d9844eb58ea8472d7ae9
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2022-02-25 15:15:54 +0530

    adds logback programatically

--- poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/Application.java
+++ poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/Application.java
@@ -2,10 +2,14 @@ package com.example.poc.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.logging.logback.ColorConverter;
+import org.springframework.nativex.hint.InitializationHint;
+import org.springframework.nativex.hint.InitializationTime;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@InitializationHint(types = ColorConverter.class, initTime = InitializationTime.BUILD)
 public class Application {
 
     public static void main(String[] args) {
--- poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/org/springframework/boot/logging/logback/CustomConfigurator.java
+++ poc-spring-boot-rest/poc-spring-boot-rest-webflux/src/main/java/org/springframework/boot/logging/logback/CustomConfigurator.java
@@ -1,4 +1,9 @@
-package com.example.poc.reactive.config;
+package org.springframework.boot.logging.logback;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.Configurator;
+import ch.qos.logback.core.spi.ContextAwareBase;
+import org.springframework.boot.logging.LoggingSystem;
 
 public class CustomConfigurator extends ContextAwareBase implements Configurator {
 
