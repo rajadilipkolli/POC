3f54196186b946292905ec6e0f24105c061151e8
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2022-04-23 09:11:35 +0530

    using SequenceGenerator instead of hibernate Generator

--- poc-spring-boot-rest/poc-spring-boot-rest-webmvc/src/main/java/com/example/poc/webmvc/entities/PostComment.java
+++ poc-spring-boot-rest/poc-spring-boot-rest-webmvc/src/main/java/com/example/poc/webmvc/entities/PostComment.java
@@ -7,10 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity(name = "PostComment")
 @Table(name = "post_comment")
@@ -19,14 +19,7 @@ import org.hibernate.annotations.GenericGenerator;
 public class PostComment {
 
     @Id
-    @GenericGenerator(
-            name = "sequenceGenerator",
-            strategy = "enhanced-sequence",
-            parameters = {
-                @org.hibernate.annotations.Parameter(name = "optimizer", value = "pooled-lo"),
-                @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
-                @org.hibernate.annotations.Parameter(name = "increment_size", value = "5")
-            })
+    @SequenceGenerator(allocationSize = 5, name = "sequenceGenerator")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     private Long id;
 
