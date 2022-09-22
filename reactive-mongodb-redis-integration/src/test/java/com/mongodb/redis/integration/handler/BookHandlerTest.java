eebf3db29ce2dc42afe1a133241833a8e8d79a12
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-22 22:27:16 +0530

    fixes imports

--- reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/handler/BookHandlerTest.java
+++ reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/handler/BookHandlerTest.java
@@ -1,7 +1,24 @@
 package com.mongodb.redis.integration.handler;
 
+import com.mongodb.redis.integration.request.BookDTO;
+import com.mongodb.redis.integration.service.ReactiveCachingService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.reactive.function.server.MockServerRequest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
-public class BookHandlerTest {
+class BookHandlerTest {
 
     @Mock
     private ReactiveCachingService reactiveCachingService;
@@ -20,8 +37,8 @@ public class BookHandlerTest {
 
     @Test
     public void testGetBook() {
-        when(reactiveCachingService.getBookById(anyString())).thenReturn(Mono.just(new BookDTO("1", "book1", "author1")));
-        StepVerifier.create(bookHandler.getBook(MockServerRequest.builder().build()))
+        when(reactiveCachingService.getBookById("1")).thenReturn(Mono.just(new BookDTO("1","book1", "author1","text",1L)));
+        StepVerifier.create(bookHandler.getBook(MockServerRequest.builder().method(HttpMethod.GET).pathVariable("id", "1").build()))
                 .expectNextMatches(serverResponse -> serverResponse.statusCode().equals(HttpStatus.OK))
                 .verifyComplete();
     }
@@ -29,7 +46,7 @@ public class BookHandlerTest {
     @Test
     public void testGetBookNotFound() {
         when(reactiveCachingService.getBookById(anyString())).thenReturn(Mono.empty());
-        StepVerifier.create(bookHandler.getBook(MockServerRequest.builder().build()))
+        StepVerifier.create(bookHandler.getBook(MockServerRequest.builder().method(HttpMethod.GET).pathVariable("id", "1").build()))
                 .expectNextMatches(serverResponse -> serverResponse.statusCode().equals(HttpStatus.NOT_FOUND))
                 .verifyComplete();
     }
@@ -37,7 +54,7 @@ public class BookHandlerTest {
     @Test
     public void testDeleteBook() {
         when(reactiveCachingService.deleteBook(anyString())).thenReturn(Mono.just(new BookDTO("1", "book1", "author1")));
-        StepVerifier.create(bookHandler.deleteBook(MockServerRequest.builder().build()))
+        StepVerifier.create(bookHandler.deleteBook(MockServerRequest.builder().method(HttpMethod.DELETE).pathVariable("id", "1").build()))
                 .expectNextMatches(serverResponse -> serverResponse.statusCode().equals(HttpStatus.ACCEPTED))
                 .verifyComplete();
     }
@@ -45,7 +62,7 @@ public class BookHandlerTest {
     @Test
     public void testDeleteBookNotFound() {
         when(reactiveCachingService.deleteBook(anyString())).thenReturn(Mono.empty());
-        StepVerifier.create(bookHandler.deleteBook(MockServerRequest.builder().build()))
+        StepVerifier.create(bookHandler.deleteBook(MockServerRequest.builder().method(HttpMethod.DELETE).pathVariable("id", "1").build()))
                 .expectNextMatches(serverResponse -> serverResponse.statusCode().equals(HttpStatus.NOT_FOUND))
                 .verifyComplete();
     }
@@ -58,4 +75,3 @@ public class BookHandlerTest {
                 .verifyComplete();
     }
 }
-
