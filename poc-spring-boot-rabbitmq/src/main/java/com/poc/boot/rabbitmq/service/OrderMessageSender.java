8ac95dc076a3cb7660f4dfaf3301af9bdfc273c8
Raja Kolli <rajadilipkolli@gmail.com>
2022-11-25 08:22:51 +0530

    removing project as it is relocated to spring experiments

--- poc-spring-boot-rabbitmq/src/main/java/com/poc/boot/rabbitmq/service/OrderMessageSender.java
+++
@@ -1,9 +0,0 @@
-package com.poc.boot.rabbitmq.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.poc.boot.rabbitmq.model.Order;
-
-public interface OrderMessageSender {
-
-    void sendOrder(Order order) throws JsonProcessingException;
-}
