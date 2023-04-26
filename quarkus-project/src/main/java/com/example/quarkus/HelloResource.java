7637fb91e5f4476999adda09227dc436450e8705
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-26 16:25:10 +0000

    migrate to jakarta namespace

--- quarkus-project/src/main/java/com/example/quarkus/HelloResource.java
+++ quarkus-project/src/main/java/com/example/quarkus/HelloResource.java
@@ -2,12 +2,12 @@ package com.example.quarkus;
 
 import com.example.quarkus.service.HelloService;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
