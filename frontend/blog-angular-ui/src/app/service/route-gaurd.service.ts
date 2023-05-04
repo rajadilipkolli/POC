6024fef46ada831072452257e1371075ec3ec92a
Raja Kolli <rajadilipkolli@gmail.com>
2023-05-04 15:36:24 +0000

    upgrade to angular 16

--- frontend/blog-angular-ui/src/app/service/route-gaurd.service.ts
+++ frontend/blog-angular-ui/src/app/service/route-gaurd.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 import { BasicAuthenticationService } from './basic-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGaurdService implements CanActivate {
+export class RouteGaurdService  {
 
   constructor(
     private hardcodedAuthenticationService: HardcodedAuthenticationService,
