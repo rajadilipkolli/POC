ec7351c984ae805fd0f912e3c2ccff4d37d4ff27
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-28 11:46:54 +0000

    fix : Integration Test

--- poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/controller/v1/ItemStreamControllerIntegrationTest.java
+++ poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/controller/v1/ItemStreamControllerIntegrationTest.java
@@ -33,6 +33,7 @@ class ItemStreamControllerIntegrationTest extends AbstractIntegrationTest {
                 ItemCapped.class, CollectionOptions.empty().maxDocuments(50).size(5000).capped());
         Flux<ItemCapped> itemCappedFlux =
                 Flux.interval(Duration.ofMillis(5))
+                        .onBackpressureBuffer()
                         .map(i -> new ItemCapped(null, "Random Item " + i, 100.00 + i))
                         .take(5);
 
