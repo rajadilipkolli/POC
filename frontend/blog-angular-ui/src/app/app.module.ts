3e11287e3415d7c21cbad60755a9dc8675a059b6
Raja Kolli <rajadilipkolli@gmail.com>
2025-06-06 14:48:00 +0000

    fix build

--- frontend/blog-angular-ui/src/app/app.module.ts
+++ frontend/blog-angular-ui/src/app/app.module.ts
@@ -5,35 +5,22 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './error/error.component';
-import { ListPostsComponent } from './list-posts/list-posts.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { LogoutComponent } from './logout/logout.component';
 import { HttpInterceptorBasicAuthService } from './service/http/http-interceptor-basic-auth.service';
-import { PostComponent } from './post/post.component';
-import { CreatePostComponent } from './create-post/create-post.component';
 import { DatePipe } from '@angular/common';
 
-@NgModule({ declarations: [
-        AppComponent,
-        LoginComponent,
-        WelcomeComponent,
-        ErrorComponent,
-        ListPostsComponent,
-        MenuComponent,
-        FooterComponent,
-        LogoutComponent,
-        PostComponent,
-        CreatePostComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
+@NgModule({
+    declarations: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
         AppRoutingModule,
-        FormsModule], providers: [
+        FormsModule,
+        AppComponent
+    ],
+    providers: [
         { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorBasicAuthService, multi: true },
         DatePipe,
         provideHttpClient(withInterceptorsFromDi())
-    ] })
+    ]
+})
 export class AppModule { }
