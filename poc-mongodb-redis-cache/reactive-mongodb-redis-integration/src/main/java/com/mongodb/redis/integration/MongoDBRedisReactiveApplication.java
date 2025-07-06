e4d5c632cdc13c59cf478ae01474e6184883ac09
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-06 11:46:38 +0000

    feat: enhance BlockHound configuration to allow specific blocking calls in MongoDB-Redis integration

--- poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/MongoDBRedisReactiveApplication.java
+++ poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/MongoDBRedisReactiveApplication.java
@@ -8,7 +8,15 @@ import reactor.blockhound.BlockHound;
 @SpringBootApplication
 public class MongoDBRedisReactiveApplication {
     public static void main(String[] args) {
-        BlockHound.install();
+        BlockHound.builder()
+                .allowBlockingCallsInside(
+                        "org.springframework.validation.beanvalidation.SpringValidatorAdapter",
+                        "validate")
+                .allowBlockingCallsInside("jakarta.validation.Validator", "validate")
+                .allowBlockingCallsInside("org.slf4j.LoggerFactory", "getLogger")
+                .allowBlockingCallsInside("java.util.UUID", "randomUUID")
+                .install();
+
         SpringApplication.run(MongoDBRedisReactiveApplication.class, args);
     }
 }
