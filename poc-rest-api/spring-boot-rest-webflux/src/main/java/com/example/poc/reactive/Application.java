e9176963cc33a2dd001b013dadfff66ba79623ad
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-06 11:58:29 +0000

    feat: update Maven Wrapper to version 3.3.2 and add BlockHound dependency for reactive application

--- poc-rest-api/spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/Application.java
+++ poc-rest-api/spring-boot-rest-webflux/src/main/java/com/example/poc/reactive/Application.java
@@ -4,10 +4,15 @@ package com.example.poc.reactive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class Application {
+
+    static {
+        BlockHound.install();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
