679b9295b14c672ad8759567c9340dd959c12700
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-06 11:39:52 +0000

    feat: update docker-compose and application configuration for reactive MongoDB-Redis integration; add BlockHound dependency

--- poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/MongoDBRedisReactiveApplication.java
+++ poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/main/java/com/mongodb/redis/integration/MongoDBRedisReactiveApplication.java
@@ -3,10 +3,12 @@ package com.mongodb.redis.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class MongoDBRedisReactiveApplication {
     public static void main(String[] args) {
+        BlockHound.install();
         SpringApplication.run(MongoDBRedisReactiveApplication.class, args);
     }
 }
