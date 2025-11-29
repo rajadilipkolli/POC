56f8c9ec91b2e175b5c086438db981b5d091ca61
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-29 16:08:06 +0000

    fix : test failures

--- poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/config/TestContainersConfig.java
+++ poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/config/TestContainersConfig.java
@@ -15,7 +15,7 @@ public class TestContainersConfig {
     @Bean
     @ServiceConnection
     public MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.0.22"))
+        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.2.2"))
                 .withSharding()
                 .withStartupAttempts(3)
                 .withStartupTimeout(Duration.ofMinutes(2))
