27b34073ad79d1afbd646f57471193c3bfda3b7e
Raja Kolli <rajadilipkolli@gmail.com>
2026-01-31 10:48:16 +0530

    upgrade version in tests

--- poc-mongodb-redis-cache/mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/config/MongoDBTestContainerConfig.java
+++ poc-mongodb-redis-cache/mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/config/MongoDBTestContainerConfig.java
@@ -10,7 +10,7 @@ public interface MongoDBTestContainerConfig {
 
     @ServiceConnection
     MongoDBContainer mongoDBContainer =
-            new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.2.2"))
+            new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.2.4"))
                     .withSharding()
                     .withStartupAttempts(3)
                     .withStartupTimeout(Duration.ofMinutes(2))
--- poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/config/TestContainersConfig.java
+++ poc-mongodb-redis-cache/reactive-mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/config/TestContainersConfig.java
@@ -15,7 +15,7 @@ public class TestContainersConfig {
     @Bean
     @ServiceConnection
     public MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.2.2"))
+        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.2.4"))
                 .withSharding()
                 .withStartupAttempts(3)
                 .withStartupTimeout(Duration.ofMinutes(2))
