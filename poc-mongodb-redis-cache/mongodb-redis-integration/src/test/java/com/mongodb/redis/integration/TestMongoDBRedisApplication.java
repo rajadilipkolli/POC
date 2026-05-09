bc7beb5b98cb118665bac5fd9e8675745e2100e2
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-09 07:27:18 +0000

    upgrade to latest versions in test

--- poc-mongodb-redis-cache/mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/TestMongoDBRedisApplication.java
+++ poc-mongodb-redis-cache/mongodb-redis-integration/src/test/java/com/mongodb/redis/integration/TestMongoDBRedisApplication.java
@@ -2,30 +2,16 @@
 package com.mongodb.redis.integration;
 
 import com.mongodb.redis.integration.config.MongoDBTestContainerConfig;
+import com.mongodb.redis.integration.config.RedisTestContainerConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.devtools.restart.RestartScope;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
 @ImportTestcontainers(MongoDBTestContainerConfig.class)
 public class TestMongoDBRedisApplication {
 
-    @Bean
-    @ServiceConnection(name = "redis")
-    @RestartScope
-    public GenericContainer redisContainer() {
-        return new GenericContainer(DockerImageName.parse("redis").withTag("8.4.0-alpine"))
-                .withExposedPorts(6379);
-    }
-
     public static void main(String[] args) {
         SpringApplication.from(MongoDBRedisApplication::main)
-                .with(TestMongoDBRedisApplication.class)
+                .with(RedisTestContainerConfig.class)
                 .run(args);
     }
 }
